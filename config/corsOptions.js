ea676306e8900e7a02f3f73c1268b49b5d927ff1
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2023-11-15 16:42:01 +0100

    update

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: (origin, callback) => {
+    origin: (origin, callback) => { 
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
